04bf92505a919dc865ade947d524ce5bed1a3aba
Andrew <defould@ya.ru>
2023-03-10 18:42:58 +0300

    ready to work

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const slides = document.querySelectorAll('.slide');
+
+slides.forEach((slide) => {
+    slide.addEventListener('click', () => {
+        clearClassActive();
+        slide.classList.add('active');
+    });
+});
+
+function clearClassActive() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+}
